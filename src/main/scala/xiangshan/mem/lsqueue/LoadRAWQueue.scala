dcfa7bf3e2578a764759525d5fa5de024264b5fb
Fawang <fwcheung@outlook.com>
2024-08-13 14:36:20 +0800

    [Topdown]: fix MemVioRedirect condition

--- src/main/scala/xiangshan/mem/lsqueue/LoadRAWQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/LoadRAWQueue.scala
@@ -285,8 +285,8 @@ class LoadRAWQueue(implicit p: Parameters) extends XSModule
   rollbackRes.bits.isXRet := false.B
   rollbackRes.bits.isFlushPipe := false.B
   rollbackRes.bits.isPreWalk := false.B
-  rollbackRes.bits.debugIsCtrl := true.B
-  rollbackRes.bits.debugIsMemVio := false.B
+  rollbackRes.bits.debugIsCtrl := false.B
+  rollbackRes.bits.debugIsMemVio := true.B
 
   io.rollback := rollbackRes
 
