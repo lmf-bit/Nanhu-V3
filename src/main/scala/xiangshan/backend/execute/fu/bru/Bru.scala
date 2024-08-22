6c1f2a7bdd5a3d91792a76f6abdb066fbecbafce
fawangZ <fwcheung@outlook.com>
2024-08-22 11:16:59 +0800

    [Topdown]: add topdown redirect type

--- src/main/scala/xiangshan/backend/execute/fu/bru/Bru.scala
+++ src/main/scala/xiangshan/backend/execute/fu/bru/Bru.scala
@@ -58,6 +58,8 @@ class Bru(implicit p: Parameters) extends FUWithRedirect {
   redirectOut.isLoadStore := false.B
   redirectOut.isFlushPipe := uop.ctrl.flushPipe
   redirectOut.isPreWalk := false.B
+  redirectOut.debugIsMemVio := false.B
+  redirectOut.debugIsCtrl := true.B
 
   io.in.ready := io.out.ready
   io.out.valid := io.in.valid
